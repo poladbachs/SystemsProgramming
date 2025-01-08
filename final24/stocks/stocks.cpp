268ddf5b431856fab4082d91754bd6e1c82ec123
Polad <bakhip@usi.ch>
2025-01-08 04:08:03 +0100

    C++ stock management library

---
+++ final24/stocks/stocks.cpp
@@ -0,0 +1,20 @@
+#include <list>
+#include <cstring>
+
+#include "stocks.h"
+
+static const int MAX_STOCK_LEN = 5;
+
+struct trade {
+    double time;
+    char symbol[MAX_STOCK_LEN + 1];
+    double price;
+    unsigned int quantity;
+};
+
+struct trades_log {
+    std::list<trade> trades;
+    double window;
+    trades_log(): window(60) {};
+    void trim();
+};
