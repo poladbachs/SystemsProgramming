58d13922ffcde74d57f11db253ef465fca9663de
Polad <bakhip@usi.ch>
2025-01-09 22:15:50 +0100

    parsing tags from message

--- final24/messaging/messaging.cpp
+++ final24/messaging/messaging.cpp
@@ -17,3 +17,27 @@ struct server * server_new() {
 void server_delete(struct server * s) {
     delete(s);
 }
+
+static void read_tags(const char * m, set<string> & s) {
+    const char * begin;
+    s.clear();
+
+    init_state:
+        if (*m == '#' || *m == '@' || isalpha(*m)) {
+            begin = m;
+            m += 1;
+            goto reading_tag;
+        }
+        if (*m == 0)
+            return;
+        m += 1;
+        goto reading_tag;
+    reading_tag:
+        if(isalpha(*m)) {
+            m += 1;
+            goto reading_tag;
+        }
+        string tag(begin, m - begin);
+        s.insert(tag);
+        goto init_state;
+}
